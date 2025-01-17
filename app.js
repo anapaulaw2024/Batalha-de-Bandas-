2a9db0804921c904f7deb48a70201dd012ab8177
AnaWulCard <156938112+anapaulaw2024@users.noreply.github.com>
2025-01-17 14:55:59 -0300

    Criação da parte interativa da página
    validação de formulário

---
+++ app.js
@@ -0,0 +1,6 @@
+let nomeBanda = getElementById('banda');
+let integrantes = getElementById('integrantes');
+let genero = getElementById('genero');
+let emailContato = getElementById('email');
+let linkApresentacao = getElementById('ytlink');
+let validar.getElementById('validar') = nomebanda + integrantes + genero + emailContato + linkApresentacao
